b971b1f38d891c89620f3b705a1e493fee965cb5
Chi Le <lehuechi04@gmail.com>
2025-03-22 23:46:22 +0100

    Add reading order print for square

--- test1.py
+++ test1.py
@@ -5,60 +5,87 @@ arr = [0] * length
 
 def main():
     if mode == "left" or mode == "l":
-        printLeft()
+        arr = [0] * length
+        printLeft(arr)
     elif mode == "right" or mode == "r":
-        printRight()
+        arr = [0] * length
+        printRight(arr)
     elif mode == "in":
-        printIn()
+        arr = [0] * length
+        printIn(arr)
     elif mode == "out":
-        printOut()
+        arr = [0] * length
+        printOut(arr)
     elif mode == "loopr":
+        arr = [0] * length
         pos = int(input("Enter position: "))
-        printLoopRight(pos)
+        printLoopRight(arr, pos)
     elif mode == "loopl":
+        arr = [0] * length
         pos = int(input("Enter position: "))
-        printLoopLeft(pos)
+        printLoopLeft(arr, pos)
     elif mode == "loopout":
+        arr = [0] * length
         pos = int(input("Enter position: "))
-        printLoopOut(pos)
+        printLoopOut(arr, pos)
+    elif mode == "reading":
+        arr = [[0 for i in range(length)] for j in range(length)]
+        # printSquare(arr)
+        print2dReading(arr)
     
-def printLeft():
+def printLeft(arr):
     for i in reversed(range(0, length)):
         arr[i] = 1
         printArray(arr)
 
-def printRight():
+def printRight(arr):
     for i in range(0, length):
         arr[i] = 1
         printArray(arr)
 
-def printOut():
+def printOut(arr):
     for k in reversed(range(0,(length+1)//2)):
         arr[k] = 1
         arr[length-1-k] = 1
         printArray(arr)
 
-def printIn():
+def printIn(arr):
     for k in range(0,(length+1)//2):
         arr[k] = 1
         arr[length-1-k] = 1
         printArray(arr)
 
-def printLoopRight(pos):
+def printLoopRight(arr, pos):
     for k in range(pos, pos+length):
         arr[k%length] = 1
         printArray(arr)
 
-def printLoopLeft(pos):
+def printLoopLeft(arr, pos):
     for k in range(pos, pos-length, -1):
         arr[k%length] = 1
         printArray(arr)
 
-def printLoopOut(pos):
+def printLoopOut(arr, pos):
     for k in range(pos, pos+(length+1)//2):
         arr[k%length] = 1
         arr[(pos*2-k)%length] = 1
         printArray(arr)
+
+def print2dReading(arr):
+    for x in range(0, len(arr)):
+        for y in range(0, len(arr)):
+            arr[x][y] = 1
+            printSquare(arr)
+
+def printSquare(arr):
+    print("square\n")
+    for x in range(0, len(arr)):
+        for y in range(0, len(arr)):
+            printBlock(arr[x][y])
+        # print(end="\r")
+        print()
+        
+    # time.sleep(0.5)
 
 def printArray(arr):
     print("[", end="")
@@ -68,8 +95,8 @@ def printArray(arr):
     print(end="\r")
     time.sleep(0.5)
 
-def printBlock(x):
-    if x == 0:
+def printBlock(a):
+    if a == 0:
         print("-", end="")
     else:
         print(chr(0x2588), end="")
