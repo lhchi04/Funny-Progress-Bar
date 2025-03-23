8fabffd8f592489635644ac941099c6bf92e54e2
Chi Le <lehuechi04@gmail.com>
2025-03-23 01:02:52 +0100

    Add 2d diagonal print func and add pretty print func

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import time
+import sys
 length = int(input("Enter array length: "))
 mode = input("Choose mode: ").lower()
 arr = [0] * length
@@ -28,29 +29,37 @@ def main():
         arr = [0] * length
         pos = int(input("Enter position: "))
         printLoopOut(arr, pos)
-    elif mode == "reading":
+    elif mode == "2drow":
         arr = [[0 for i in range(length)] for j in range(length)]
-        # printSquare(arr)
-        print2dReading(arr)
+        print2dRow(arr)
+    elif mode == "2dcol":
+        arr = [[0 for i in range(length)] for j in range(length)]
+        print2dCol(arr)
+    elif mode == "2dzigzag":
+        arr = [[0 for i in range(length)] for j in range(length)]
+        print2dZigzag(arr)
+    elif mode == "2ddiagonal":
+        arr = [[0 for i in range(length)] for j in range(length)]
+        print2dDiagonal(arr)
     
 def printLeft(arr):
-    for i in reversed(range(0, length)):
+    for i in reversed(range(len(arr))):
         arr[i] = 1
         printArray(arr)
 
 def printRight(arr):
-    for i in range(0, length):
+    for i in range(len(arr)):
         arr[i] = 1
         printArray(arr)
 
 def printOut(arr):
-    for k in reversed(range(0,(length+1)//2)):
+    for k in reversed(range((length+1)//2)):
         arr[k] = 1
         arr[length-1-k] = 1
         printArray(arr)
 
 def printIn(arr):
-    for k in range(0,(length+1)//2):
+    for k in range((length+1)//2):
         arr[k] = 1
         arr[length-1-k] = 1
         printArray(arr)
@@ -71,21 +80,58 @@ def printLoopOut(arr, pos):
         arr[(pos*2-k)%length] = 1
         printArray(arr)
 
-def print2dReading(arr):
-    for x in range(0, len(arr)):
-        for y in range(0, len(arr)):
+def print2dRow(arr):
+    for x in range(len(arr)):
+        for y in range(len(arr)):
             arr[x][y] = 1
             printSquare(arr)
 
+def print2dCol(arr):
+    for x in range(len(arr)):
+        for y in range(len(arr)):
+            arr[y][x] = 1
+            printSquare(arr)
+
+def print2dZigzag(arr):
+    for x in range(len(arr)):
+        if x%2 == 0: # odd row: print left to right
+            for y in range(len(arr)):
+                arr[x][y] = 1
+                printSquare(arr)
+        else: # even row: print right to left
+            for y in reversed(range(len(arr))):
+                arr[x][y] = 1
+                printSquare(arr)
+
+def print2dDiagonal(arr):
+    for i in range(len(arr)):
+        x = i
+        y = 0
+        for j in range(i+1):
+            arr[x][y] = 1
+            printSquare(arr)
+            x -= 1
+            y += 1
+    z = len(arr)-1
+    for i in range(1, len(arr)):
+        y = i
+        x = len(arr)-1
+        for j in range(z):
+            arr[x][y] = 1
+            printSquare(arr)
+            x -= 1
+            y += 1
+        z -= 1
+
+# def print2dAntiDiagonal(arr):
+
 def printSquare(arr):
-    print("square\n")
     for x in range(0, len(arr)):
         for y in range(0, len(arr)):
             printBlock(arr[x][y])
-        # print(end="\r")
         print()
-        
-    # time.sleep(0.5)
+    delete_multiple_lines(len(arr))
+    time.sleep(0.5)
 
 def printArray(arr):
     print("[", end="")
@@ -100,5 +146,11 @@ def printBlock(a):
         print("-", end="")
     else:
         print(chr(0x2588), end="")
+
+def delete_multiple_lines(n):
+    """Delete the last line in the STDOUT."""
+    for i in range(n):
+        sys.stdout.write("\x1b[1A")  # cursor up one line
+        sys.stdout.write("\x1b[2K")  # delete the last line
 
 main()
